7a77fca8273c07f29a93f932b9349447c0b16247
AriaGT <ariagt191000@gmail.com.com>
2022-11-03 16:48:03 -0500

    Se terminan los efectos del modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,12 +5,19 @@ onSubmit = document.querySelector('.button_submit')
 
 closeBtn.addEventListener('click', () => {
   modal.classList.toggle('modal_active')
+  setTimeout(() => {
+    modal.classList.toggle('hiden')
+  }, 200)
 })
 
 openBtn.addEventListener('click', () => {
   modal.classList.toggle('modal_active')
+  modal.classList.toggle('hiden')
 })
 
 onSubmit.addEventListener('click', () => {
   modal.classList.toggle('modal_active')
+  setTimeout(() => {
+    modal.classList.toggle('hiden')
+  }, 200)
 })
